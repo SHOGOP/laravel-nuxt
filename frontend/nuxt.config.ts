22ac421f6cdcba98135a9fc06731b1b330bf841b
SHOGOP <shou.wktk@gmail.com>
2024-04-27 04:44:59 +0000

    @nuxtjs/axios @nuxtjs/proxy install

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -3,5 +3,11 @@ export default defineNuxtConfig({
   modules: [
     '@invictus.codes/nuxt-vuetify',
     ['@nuxtjs/eslint-module', { lintOnStart: false }],
+    '@nuxtjs/axios',
+    '@nuxtjs/proxy'
   ],
+  axios: { proxy: true },
+proxy: {
+  '/api': 'http://127.0.0.1:8000',
+},
 });
